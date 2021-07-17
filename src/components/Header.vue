d6c4c85383357ef9c4b6115a2ec877dcb5d9a8b5
MattiaR <rovitomattia91@gmail.com>
2021-07-17 09:29:27 +0200

    Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,47 @@
- <template>
-   <h1> Prova prova prova </h1>
- </template>
+<template>
+    <header class="d-flex justify-content-between p-3">
+        <div class="p-2">
+            <img src="@/assets/img/logo-sidearea-1.png" alt="Logo">
+        </div>
+        
+        <div class="d-flex">
+            <ul class="d-flex text-uppercase fw-bold">
+                <li v-for="(element, i) in menu" :key="i" class="m-3"><a href="#">{{element}}</a>
+               
+                </li>
+            </ul>
+            <div class="mx-2 p-1 mt-2">
+                <a href="#"><img src="@/assets/svg/svg-1.svg" alt="menu"></a>
+            </div>
+           
+            
+        </div>
+      
+    </header>
+</template>
  
- <script>
- export default {
-     name: "Header",
- }
- </script>
- 
- <style lang='scss' scoped>
- 
- </style>
+<script>
+export default {
+    name: 'Header',
+    data (){
+        return {
+            menu : ['Home', 'Pages', 'Portfolio', 'Blog', 'Shop', 'Elements']
+        }
+       
+    }
+}
+</script>
+
+<style lang="scss">
+
+li
+{
+    font-size: 14px;
+}
+
+
+
+
+
+
+</style>

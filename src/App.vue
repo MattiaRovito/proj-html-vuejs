10ff10b0f61676828e442c925f609d3316baa3e2
MattiaR <rovitomattia91@gmail.com>
2021-07-19 21:36:46 +0200

    qode position fixed

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,38 @@
 <template>
   <div id="app">
+    
+      <div class="qode d-flex">
+        <img src="@/assets/svg/svg-4.svg" alt="svg">
+        <span class="text-uppercase related">Related</span>
+      </div>
+    
+
+    <!-- <div class="shop d-flex">
+      <img src="@/assets/svg/svg-9.svg" alt="svg">
+      <span class="text-uppercase related">Buy Now</span>
+    </div> -->
+    
+
     <Header/>
 
     <Main/>
+
+    <Footer/>
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
 import Main from './components/Main.vue'
+import Footer from './components/Footer.vue'
+
 
 export default {
   name: 'App',
   components: {
     Header,
-    Main
+    Main,
+    Footer
   }
 }
 </script>
@@ -24,6 +42,24 @@ export default {
 
 
 
+
+.qode
+{
+  width: 2%;
+  position:fixed;
+  right: 80px;
+  top: 170px;
+  background-color: red;
+  z-index: 1000;
+}
+
+.related
+{
+  line-height: 3;
+  margin-left: 8px;
+  font-size: 14px;
+  
+}
 
 
 
---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer>
+      <div class="container">
+          CIAO
+      </div>
+  </footer>
+</template>
+
+<script>
+export default {
+name: 'Footer'
+}
+</script>
+
+<style lang='scss' scoped>
+footer
+{
+    height: 228px;
+    margin-top: 150px;
+    background-color: #c0e1cf;
+}
+</style>

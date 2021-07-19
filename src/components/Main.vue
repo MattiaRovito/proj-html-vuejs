a9a268e2ab132a46e217073934c982ab64c646a0
MattiaR <rovitomattia91@gmail.com>
2021-07-19 19:46:42 +0200

    add components Posters

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,13 +2,15 @@
     <div>
         <div class="row m-0 p-0">
 
-       
-        <Illustration class="col-4 p-0 m-0" v-for="(pic, i) in pics" :key='i'
+            <Illustration 
+            class="col-4 p-0 m-0" 
+            v-for="(pic, i) in pics" 
+            :key='i'
             :img='pic.image'
             :txt='pic.text'
             :id='pic.id'
-            
             />
+        
 
         </div>
 
@@ -16,7 +18,19 @@
 
         <Message/>
 
-        <Posters/>
+        <div class="row justify-content-center m-0">
+
+            <Posters
+            class="col-3"
+            v-for="(poster,i) in posters" 
+            :key="i"
+            :imgP="poster.imgPos"
+            :txtP="poster.textPos"
+            :idP="poster.idPos"
+            :dayP="poster.dayPos"
+            :monthP="poster.monthPos"/>
+
+        </div>
     </div>
    
 </template>
@@ -70,6 +84,29 @@ export default {
                     id: 'Illustration'
                 }
                 
+            ],
+            posters: [
+                {
+                    imgPos: 'h-2-blog-img-1',
+                    textPos: 'This way, loves: a detailed guide through new design',
+                    idPos: 'By Emily Fields',
+                    dayPos: '07',
+                    monthPos: "May '19"
+                },
+                {
+                    imgPos: 'h-2-blog-img-2',
+                    textPos: 'I try to give people a different way of looking art',
+                    idPos: 'By Emily Fields',
+                    dayPos: '07',
+                    monthPos: "May '19"
+                },
+                {
+                    imgPos: 'h-2-blog-img-3',
+                    textPos: 'Introduce Richard Laperrière of those amazing features',
+                    idPos: 'By Emily Fields',
+                    dayPos: '07',
+                    monthPos: "May '19"
+                },
             ]                   
         }
     }
--- src/components/Posters.vue
+++ src/components/Posters.vue
@@ -1,15 +1,92 @@
 <template>
-  <div>
-
+  <div class="position-relative">
+  
+   
+    <div class="margin">
+      <div class="box-position">
+        <div class="box-top fst-italic fw-bold">{{dayP}}</div>
+        <div class="box-bottom text-uppercase">{{monthP}}</div>
+      </div>
+      
+      <img :src="require('@/assets/img/' + imgP + '.jpg')" :alt="imgP" class="w-100 box-img">
+    </div>
+    
+    <div class="h4 mt-3"><a href="#">{{txtP}}</a></div>
+    <span class="fst-italic">{{idP}}</span>
   </div>
 </template>
 
 <script>
 export default {
-    name: 'Posters'
+    name: 'Posters',
+    props: {
+      imgP: Image,
+      txtP: String,
+      idP: String,
+      dayP: Number,
+      monthP: String
+    }
 }
 </script>
 
 <style lang="scss" scoped>
+
+.box-position
+{
+  position:absolute;
+  top: 130px;
+  left: 30px;
+  z-index: 99;
+}
+
+.box-top
+{
+  font-size: 26px;
+  background-color: #c0e1cf;
+  width: 100%;
+  padding: 4px 15px;
+}
+.box-bottom
+{
+  font-size: 14px;
+  background-color: #f3d0c7;
+  width: 100%;
+  height: 38px;
+  clip-path: polygon(0 0,100% 0,100% 100%,50% 70%,0 100%);
+  padding: 3px 8px;
+}
+.margin
+{
+  margin-top: 150px;
+}
+
+
+img
+{
+  &:hover
+  {
+    transform: scale(1.009);
+  }
+}
+
+
+
+.h4
+{
+   a::after
+    {
+    content: "";
+    width: 0;
+    height: 3px;
+    background-color: rgb(34, 156, 34);
+    display: block;
+    transition: width 0.5s;
+    }
+
+    a:hover::after 
+    {
+    width: 100%;
+    }
+}
 
 </style>

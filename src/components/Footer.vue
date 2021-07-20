46c807b485acec0ff9fbc51dc88d67f48de2d42c
MattiaR <rovitomattia91@gmail.com>
2021-07-20 16:22:10 +0200

    add footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,14 +1,36 @@
 <template>
   <footer>
-      <div class="container">
+      <div class="container text-center">
+        <nav>
+          <ul class="row p-3">
+            <li class="col-3 mb-2"><img src="@/assets/img/logo-sidearea-1.png" alt="logo"></li>
+            <li class="col-3">maree.qode@gmail.com</li>
+            <li class="col-3">Avenue d'Auderghem 10</li>
+            <li class="col-3">Stay in touch with us</li>
+            <li class="col-3">Let's Get Creative</li>
+            <li class="col-3">+44645 321 789</li>
+            <li class="col-3">1040 Brussels, Belgium</li>
+            <li class="col-3 d-flex"><Social v-for="(social, i) in socials" :key="i" :social='social'/></li>
+          </ul>
+        </nav>
+       
           
       </div>
   </footer>
 </template>
 
 <script>
+import Social from '@/components/Social.vue'
 export default {
-name: 'Footer'
+name: 'Footer',
+components:{
+  Social
+},
+data(){
+  return {
+    socials: ['fab fa-twitter','fab fa-pinterest-p','fab fa-facebook-f','fab fa-linkedin-in']
+  }
+}
 }
 </script>
 
@@ -18,5 +40,10 @@ footer
     height: 228px;
     margin-top: 150px;
     background-color: #c0e1cf;
+    padding: 80px 100px;
+
+
 }
+
+
 </style>
---
+++ src/components/Social.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="ms-1 ps-5">
+       <a href="#"><i :class="social"></i></a>
+  </div>
+</template>
+
+<script>
+export default {
+name: 'Social',
+props: {
+    social: String
+}
+}
+</script>
+
+<style lang='scss' scoped>
+
+</style>
